727ffe1bf07e8ef2d6bb588d203c2ac3d34dac36
widiar <awidiarsana.aw@gmail.com>
2021-05-07 22:57:09 +0800

    init splash screen

---
+++ app/src/main/java/com/example/docbot/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.docbot.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.docbot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/docbot/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.docbot.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.docbot.R
+import com.example.docbot.ui.home.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
