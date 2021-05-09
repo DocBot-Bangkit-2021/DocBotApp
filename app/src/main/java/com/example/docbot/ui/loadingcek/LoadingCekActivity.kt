4b8fc7291ab80b3bedfdfa30a3f09f5d2a7e2357
Dian Arisma <dianarisma86@gmail.com>
2021-05-09 15:02:52 +0700

    add loading check activity

---
+++ app/src/main/java/com/example/docbot/ui/loadingcek/LoadingCekActivity.kt
@@ -0,0 +1,12 @@
+package com.example.docbot.ui.loadingcek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.docbot.R
+
+class LoadingCekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_cek)
+    }
+}
