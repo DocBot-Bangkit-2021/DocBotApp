62dd4dee71c1e29fca98e4d45ed7351ce2da03ec
Dian Arisma <dianarisma86@gmail.com>
2021-05-16 02:29:37 +0700

    add account activity

---
+++ app/src/main/java/com/example/docbot/ui/account/AccountResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.docbot.ui.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.docbot.R
+
+class AccountResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_result)
+    }
+}
